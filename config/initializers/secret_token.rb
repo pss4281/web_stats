9212be7099dc5da111d0b3562f40f0949f70931e
Paul Ser <pashka4281@gmail.com>
2012-06-16 01:58:45 +0300

    itim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webstats::Application.config.secret_token = '2e0ac5d457646801f4910340ab6ef0411c5296fd3525f714602a6154a0f5f3136b15ad5c6539e3ce334ac201f2213337a84c1666838e020bb4876d55fd8abb80'
